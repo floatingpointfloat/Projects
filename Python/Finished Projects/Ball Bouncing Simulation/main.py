f244bce785ff1df95afe40eae7a1fe0708c36bfe
Ryan Evertz <ryan@evertz-online.net>
2026-06-29 17:10:11 +0200

    formatting

--- Python/Finished Projects/Ball Bouncing Simulation/main.py
+++ Python/Finished Projects/Ball Bouncing Simulation/main.py
@@ -8,19 +8,15 @@ screen = pygame.display.set_mode((1000, 1000))
 pygame.display.set_caption("Bouncing Ball Simulation")
 clock = pygame.time.Clock()
 
-#variables
+# variables
 boundary = pygame.draw.circle(screen, (255, 255, 255), (500, 500), 500)
 G = Vector2(0, 1000)  # Gravitational constant for the simulation
 spawning_mass = 10
 
+
 class Ball:
-    __slots__ = ['position',
-    'velocity',
-    'acceleration',
-    'radius',
-    'color',
-    'mass']
-    
+    __slots__ = ["position", "velocity", "acceleration", "radius", "color", "mass"]
+
     def __init__(self, position, velocity, radius, color, mass):
         self.position = Vector2(position)
         self.velocity = Vector2(velocity)
@@ -40,7 +36,9 @@ class Ball:
         normal = distance_from_center.normalize()
 
         if dist + self.radius >= 500 and self.velocity.dot(normal) > 0:
-            self.velocity = self.velocity.reflect(normal) * 0.97  # Dampen the velocity to simulate energy loss
+            self.velocity = (
+                self.velocity.reflect(normal) * 0.97
+            )  # Dampen the velocity to simulate energy loss
             self.position = center + normal * (500 - self.radius)
 
     def collision(self, other):
@@ -60,7 +58,7 @@ class Ball:
             impulse = impulse_scalar * normal
             self.velocity += impulse / self.radius
             other.velocity -= impulse / other.radius
-            
+
             penetration = self.radius + other.radius - distance.length()
             self.position += normal * (
                 penetration * (other.mass / (self.mass + other.mass))
@@ -69,34 +67,58 @@ class Ball:
                 penetration * (self.mass / (self.mass + other.mass))
             )
 
+
 obj = []
 while True:
     dt = clock.tick(60) / 1000  # Time in seconds since last frame
     for event in pygame.event.get():
-        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+        if event.type == pygame.QUIT or (
+            event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE
+        ):
             print(len(obj))
             pygame.quit()
             exit()
         if event.type == pygame.MOUSEBUTTONDOWN:
             pos = Vector2(pygame.mouse.get_pos())
-            obj.append(Ball(pos, (0,0), 10, (random.randint(10, 255), random.randint(10, 255), random.randint(10, 255)), spawning_mass))
+            obj.append(
+                Ball(
+                    pos,
+                    (0, 0),
+                    10,
+                    (
+                        random.randint(10, 255),
+                        random.randint(10, 255),
+                        random.randint(10, 255),
+                    ),
+                    spawning_mass,
+                )
+            )
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 obj.clear()
-        
-    
+
     screen.fill((0, 0, 0))
     pygame.draw.circle(screen, (255, 255, 255), (500, 500), 500)
     for ball in obj:
         ball.update()
-        pygame.draw.circle(screen, ball.color, (int(ball.position.x), int(ball.position.y)), ball.radius)
-    
+        pygame.draw.circle(
+            screen,
+            ball.color,
+            (int(ball.position.x), int(ball.position.y)),
+            ball.radius,
+        )
+
     for _ in range(3):
         for i in range(len(obj)):
             for j in range(i + 1, len(obj)):
-                if obj[i].position.distance_to(obj[j].position) <= obj[i].radius + obj[j].radius:
+                if (
+                    obj[i].position.distance_to(obj[j].position)
+                    <= obj[i].radius + obj[j].radius
+                ):
                     obj[i].collision(obj[j])
-        
+
     pygame.display.update()
     fps = clock.get_fps()
-    pygame.display.set_caption(f"Bouncing Ball Simulation | Bodies: {len(obj)} | FPS: {round(fps, 1)}")
+    pygame.display.set_caption(
+        f"Bouncing Ball Simulation | Bodies: {len(obj)} | FPS: {round(fps, 1)}"
+    )
--- Python/Not finished/N body/main.py
+++ Python/Not finished/N body/main.py
@@ -4,28 +4,29 @@ import time
 import pygame
 from sys import exit
 import moderngl
-import glm 
+import glm
 
-#setup
+# setup
 ti.init(arch=ti.gpu)
 pygame.init()
 WIDTH, HEIGHT = 1000, 1000
-print("GL Version:",
-      pygame.display.gl_get_attribute(
-          pygame.GL_CONTEXT_MAJOR_VERSION
-      ),
-      pygame.display.gl_get_attribute(
-          pygame.GL_CONTEXT_MINOR_VERSION
-      ))
+print(
+    "GL Version:",
+    pygame.display.gl_get_attribute(pygame.GL_CONTEXT_MAJOR_VERSION),
+    pygame.display.gl_get_attribute(pygame.GL_CONTEXT_MINOR_VERSION),
+)
 pygame.display.gl_set_attribute(pygame.GL_CONTEXT_MAJOR_VERSION, 3)
 pygame.display.gl_set_attribute(pygame.GL_CONTEXT_MINOR_VERSION, 3)
-pygame.display.gl_set_attribute(pygame.GL_CONTEXT_PROFILE_MASK, pygame.GL_CONTEXT_PROFILE_CORE)
+pygame.display.gl_set_attribute(
+    pygame.GL_CONTEXT_PROFILE_MASK, pygame.GL_CONTEXT_PROFILE_CORE
+)
 screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.OPENGL | pygame.DOUBLEBUF)
 pygame.display.set_caption("N-Body Simulation")
 ctx = moderngl.create_context(require=330)
 ctx.enable(moderngl.BLEND)
 ctx.enable(moderngl.PROGRAM_POINT_SIZE)
 ctx.blend_func = (moderngl.SRC_ALPHA, moderngl.ONE_MINUS_SRC_ALPHA)
+
 
 @ti.data_oriented
 class Simulation:
@@ -41,7 +42,7 @@ class Simulation:
         self.active_bodies[None] = 0
 
         self.G = 5
-        self.dt = 1/60
+        self.dt = 1 / 60
 
     @ti.kernel
     def init_particles(self, num_bodies: ti.i32):
@@ -49,7 +50,7 @@ class Simulation:
             self.positions[i] = [
                 ti.random() * WIDTH / 4,
                 ti.random() * HEIGHT / 4,
-                ti.random() * 400
+                ti.random() * 400,
             ]
             self.velocities[i] = [0, 0, 0]
             self.accelerations[i] = [0, 0, 0]
@@ -64,14 +65,14 @@ class Simulation:
                 direction = self.positions[j] - self.positions[i]
                 distance = direction.norm() + 1e-3  # avoid singularity
 
-                force_magnitude = (
-                    self.G * self.masses[i] * self.masses[j]
-                ) / (distance * distance)
+                force_magnitude = (self.G * self.masses[i] * self.masses[j]) / (
+                    distance * distance
+                )
 
                 force_direction = direction / distance
                 force = force_magnitude * force_direction
-                
-                if force.norm() > 100: # cap the force to prevent numerical instability
+
+                if force.norm() > 100:  # cap the force to prevent numerical instability
                     force = 100 * force_direction
 
                 self.accelerations[i] += force / self.masses[i]
@@ -90,60 +91,67 @@ class Simulation:
     def physics_step(self):
         self.compute_gravity()
         self.update()
-      
+
 
 sim = Simulation()
 sim.init_particles(10000)
 
-class Camera: #3d
+
+class Camera:  # 3d
     def __init__(self):
         self.position = np.array([0, 0, 0], dtype=np.float32)
         self.yaw = 0
         self.pitch = 0
         self.speed = 250
-    
+
     def update(self, dt, event):
-        direction_looking = np.array([
-            np.cos(self.pitch) * np.sin(self.yaw),
-            np.sin(self.pitch),
-            np.cos(self.pitch) * np.cos(self.yaw)
-        ], dtype=np.float32)
-        right = np.array([
-            np.sin(self.yaw - np.pi/2),
-            0,
-            np.cos(self.yaw - np.pi/2)
-        ], dtype=np.float32)
-        up = np.cross(right, direction_looking) 
-        
-        if event == 'forward':
+        direction_looking = np.array(
+            [
+                np.cos(self.pitch) * np.sin(self.yaw),
+                np.sin(self.pitch),
+                np.cos(self.pitch) * np.cos(self.yaw),
+            ],
+            dtype=np.float32,
+        )
+        right = np.array(
+            [np.sin(self.yaw - np.pi / 2), 0, np.cos(self.yaw - np.pi / 2)],
+            dtype=np.float32,
+        )
+        up = np.cross(right, direction_looking)
+
+        if event == "forward":
             self.position += direction_looking * self.speed * dt
-        if event == 'backward':
+        if event == "backward":
             self.position -= direction_looking * self.speed * dt
-        if event == 'left':
+        if event == "left":
             self.position -= right * self.speed * dt
-        if event == 'right':
+        if event == "right":
             self.position += right * self.speed * dt
-        if event == 'up':
+        if event == "up":
             self.position += up * self.speed * dt
-        if event == 'down':
+        if event == "down":
             self.position -= up * self.speed * dt
-    
+
     def get_view_matrix(self):
         direction_looking = glm.vec3(
             np.cos(self.pitch) * np.sin(self.yaw),
             np.sin(self.pitch),
-            np.cos(self.pitch) * np.cos(self.yaw)
+            np.cos(self.pitch) * np.cos(self.yaw),
         )
-        direction_looking = glm.normalize(direction_looking) #normalizing the direction vector
+        direction_looking = glm.normalize(
+            direction_looking
+        )  # normalizing the direction vector
         target = self.position + direction_looking
         up = np.array([0, 1, 0], dtype=np.float32)
         return glm.lookAt(glm.vec3(*self.position), glm.vec3(*target), glm.vec3(*up))
-    
+
     def get_projection_matrix(self):
         aspect_ratio = WIDTH / HEIGHT
         return glm.perspective(glm.radians(45), aspect_ratio, 0.1, 10000.0)
 
+
 camera = Camera()
+
 
 class Input:
     def __init__(self):
@@ -153,8 +161,8 @@ class Input:
         self.locked = True
         pygame.event.set_grab(True)
         pygame.mouse.set_visible(False)
-        pygame.mouse.get_rel() #reset mouse movement to prevent jumps on first frame
-    
+        pygame.mouse.get_rel()  # reset mouse movement to prevent jumps on first frame
+
     def update(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -177,43 +185,44 @@ class Input:
                     sim.active_bodies[None] = 0
                     sim.init_particles(10000)
 
-
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w]:
-            camera.update(sim.dt, 'forward')
+            camera.update(sim.dt, "forward")
         if keys[pygame.K_s]:
-            camera.update(sim.dt, 'backward')
+            camera.update(sim.dt, "backward")
         if keys[pygame.K_a]:
-            camera.update(sim.dt, 'left')
+            camera.update(sim.dt, "left")
         if keys[pygame.K_d]:
-            camera.update(sim.dt, 'right')
+            camera.update(sim.dt, "right")
         if keys[pygame.K_SPACE]:
-            camera.update(sim.dt, 'up')
+            camera.update(sim.dt, "up")
         if keys[pygame.K_LSHIFT]:
-            camera.update(sim.dt, 'down')
+            camera.update(sim.dt, "down")
         if keys[pygame.K_LCTRL]:
             camera.speed = 2000
         if not keys[pygame.K_LCTRL]:
             camera.speed = 250
-            
+
         if not self.locked:
             return
-        
+
         pygame.mouse.set_pos(WIDTH // 2, HEIGHT // 2)
-                    
-        #self.mouse_pos = pygame.mouse.get_pos()
+
+        # self.mouse_pos = pygame.mouse.get_pos()
         dx, dy = pygame.mouse.get_rel()
         camera.yaw -= dx * self.sensitivity
         camera.pitch -= dy * self.sensitivity
         limit = np.radians(89)
         camera.pitch = max(-limit, min(limit, camera.pitch))
 
+
 input_handler = Input()
+
 
 class Renderer:
     def __init__(self):
         self.prog = ctx.program(
-            vertex_shader='''
+            vertex_shader="""
                 #version 330
                 in vec3 in_position;
                 uniform mat4 view;
@@ -223,8 +232,8 @@ class Renderer:
                     gl_Position = projection * viewPos;
                     gl_PointSize = clamp(1000.0 / -viewPos.z, 0.1, 10.0);
                 }
-            ''',
-            fragment_shader='''
+            """,
+            fragment_shader="""
                 #version 330
 
         out vec4 fragColor;
@@ -246,27 +255,28 @@ class Renderer:
 
             fragColor = vec4(color, glow);
         }
-            '''
+            """,
         )
         self.vbo = ctx.buffer(reserve=sim.max_bodies * 12)
         self.positions_np = np.empty((sim.max_bodies, 3), dtype=np.float32)
-        self.vao = ctx.simple_vertex_array(self.prog, self.vbo, 'in_position')
-    
+        self.vao = ctx.simple_vertex_array(self.prog, self.vbo, "in_position")
+
     def render(self):
         self.positions_np[:] = sim.positions.to_numpy()
         self.vbo.orphan()
         self.vbo.write(self.positions_np.tobytes())
-        self.prog['view'].write(camera.get_view_matrix().to_bytes())
-        self.prog['projection'].write(camera.get_projection_matrix().to_bytes())
+        self.prog["view"].write(camera.get_view_matrix().to_bytes())
+        self.prog["projection"].write(camera.get_projection_matrix().to_bytes())
         ctx.clear(0, 0, 0)
         self.vao.render(mode=moderngl.POINTS)
 
+
 renderer = Renderer()
-        
+
 if __name__ == "__main__":
     while True:
         input_handler.update()
         sim.physics_step()
         renderer.render()
-        
+
         pygame.display.flip()
