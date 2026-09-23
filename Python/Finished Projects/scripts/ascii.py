acd705ba902c4eb0f4d0e3e68bd1b91d5e594c7b
BlackChaos17 <m4x@mail.de>
2026-09-23 16:37:34 +0200

    fix pathname copy and looping problem

--- Python/Finished Projects/scripts/ascii.py
+++ Python/Finished Projects/scripts/ascii.py
@@ -1,58 +1,71 @@
 import PIL.Image
 
-#used ascii characters
+# used ascii characters
 ASCII_CHARS = [" ", " ", ".", ",", ":", ";", "+", "*", "?", "%", "#", "S", "@"]
 
-#function that resizes the image to the correct format
+
+# function that resizes the image to the correct format
 def resize_image(image, new_width):
     width, height = image.size
-    ratio = height / width #find the ratio to allow the height to be adjusted according to the new_width 
-    new_height = int(new_width * ratio * 0.5) 
+    ratio = (
+        height / width
+    )  # find the ratio to allow the height to be adjusted according to the new_width
+    new_height = int(new_width * ratio * 0.5)
     resized_image = image.resize((new_width, new_height))
     return resized_image
 
-#function that turnes each pixel into grayscale
+
+# function that turnes each pixel into grayscale
 def grayscale(image):
-    grayscale_image = image.convert("L") #L is the intensity of the conversion - L for grayscale :)
+    grayscale_image = image.convert(
+        "L"
+    )  # L is the intensity of the conversion - L for grayscale :)
     return grayscale_image
 
-#function to convert each "grayscaled_pixel" into the corresponding ascii char
+
+# function to convert each "grayscaled_pixel" into the corresponding ascii char
 def characterize(image):
-    pixels = image.getdata() #returns a list with the grayscale value of each pixel
-    characters = "".join([ASCII_CHARS[pixel * len(ASCII_CHARS) // 256] for pixel in pixels]) 
-    #return the corresponding ascii char 
-    #from the list for each pixel in the created pixel list
+    pixels = image.getdata()  # returns a list with the grayscale value of each pixel
+    characters = "".join(
+        [ASCII_CHARS[pixel * len(ASCII_CHARS) // 256] for pixel in pixels]
+    )
+    # return the corresponding ascii char
+    # from the list for each pixel in the created pixel list
     return characters
 
-def main(new_width=160): 
+
+def main(new_width=160):
     while True:
-        #attempt to open the image from the user input (path)
-        path = input("Enter a valid pathname to the desired image: ")
-    
+        # attempt to open the image from the user input (path)
+        path = input("Enter a valid pathname to the desired image: ").replace('"', "")
         try:
             image = PIL.Image.open(path)
         except:
             print(f"""{path} ist not a valid pathname to an image :(
             Gotta do better next time.""")
             continue
-    
-        #convert image to ascii
+
+        # convert image to ascii
         new_image_data = characterize(grayscale(resize_image(image, new_width)))
-    
-        #format
+
+        # format
         pixels_count = len(new_image_data)
-        ascii_image = "\n".join(new_image_data[i:(i+new_width)] for i in range(0, pixels_count, new_width))
-    
+        ascii_image = "\n".join(
+            new_image_data[i : (i + new_width)]
+            for i in range(0, pixels_count, new_width)
+        )
+
         print(ascii_image)
-    
+
         with open("ascii_art.txt", "w") as f:
             f.write(ascii_image)
-            
-        restart = input("\nNext image? (Y/n)")
-        if upper(restart) == "Y":
+
+        restart = input("\nNext image? (Y/n) ")
+        if restart == "Y":
             pass
         else:
             break
+
 
 if __name__ == "__main__":
     main()
